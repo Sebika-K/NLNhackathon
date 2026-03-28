10d8d840ed211d1952f749dfa22cb926ae6aed97
Sebika-K <khulalsebika11@gmail.com>
2026-03-28 14:39:53 -0500

    link dream page

--- frontend/App.js
+++ frontend/App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import FeelingsScreen from './screens/FeelingsScreen';
+import SapanaScreen from './screens/SapanaScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Feelings" component={FeelingsScreen} />
+        <Stack.Screen name="Sapana" component={SapanaScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
